59f82f8d246368570fa9cdb3e2e5b66e64b8a977
MattiaSalve <mattiasalvetti@Mattias-MacBook-Air.local>
2024-03-06 11:41:45 +0100

    Finished the part of the program that creates files and adds to old ones and made the script to execute it

---
+++ highlights.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+cd Desktop
+mv My\ Clippings.txt ~/Documents/scripts/highlight_extractor
+cd ~/Documents/scripts/highlight_extractor
+python3 highlights.py
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import re
+import os
 import pandas as pd
 
-pattern = r"""
+highlights_pattern = r"""
 (?P<title>[ \w]*)
 \ \(
 (?P<author>[\w ]*)
@@ -13,10 +14,10 @@ pattern = r"""
 """
 
 
-def extract(filename, extractedPattern):
+def extract(filename, extracted_pattern):
     df = pd.DataFrame(columns=["book", "author", "quote"])
     content = open(filename, "r").read()
-    for item in re.finditer(extractedPattern, content, re.VERBOSE):
+    for item in re.finditer(extracted_pattern, content, re.VERBOSE):
         quote = pd.DataFrame(
             {
                 "book": str(item.group(1)),
@@ -29,4 +30,37 @@ def extract(filename, extractedPattern):
     return df
 
 
-print(extract("My Clippings.txt", pattern))
+def write_to_existing_file(file, new_quote):
+    f = open(file, "r")
+    text = f.read()
+    text = text + "\n \n " + new_quote
+    f.close()
+    f = open(file, "w")
+    f.write(text)
+    f.close
+
+
+def write_to_new_file(folder, new_quote):
+    f = open(folder + "/" + new_quote["book"] + ".md", "w")
+    text = (
+        "#BookQuotes\n\n# "
+        + new_quote["book"]
+        + "\n\n### ~ "
+        + new_quote["author"]
+        + "\n \n "
+        + new_quote["quote"]
+    )
+    f.write(text)
+
+
+path = "/Users/mattiasalvetti/Library/Mobile Documents/iCloud~md~obsidian/Documents/MyVault/Book quotes"
+quotes = extract("My Clippings.txt", highlights_pattern)
+readBooks = os.listdir(path)
+
+for index, row in quotes.iterrows():
+    if row["book"] + ".md" not in readBooks:
+        write_to_new_file(path, row)
+        readBooks = os.listdir(path)
+    else:
+        file_path = path + "/" + row["book"] + ".md"
+        write_to_existing_file(file_path, row["quote"])
